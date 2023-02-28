8febc20d1fcda19d9635a454d3cf18af03d91585
Emilie <emilie.rich4rd@gmail.com>
2023-02-28 11:44:42 +0100

    state learning

--- src/components/Chocolates.vue
+++ src/components/Chocolates.vue
@@ -28,44 +28,16 @@
 </template>
 
 <script>
-import chocoBlanc from '../assets/white-chocolate-g84a4d414d_1280.jpg'
-import chocoBlack from  '../assets/chocolate-gbeab8f4b2_1280.jpg'
-import chocoMilk from '../assets/chocolate-g2e38e04ff_1280.jpg'
-
 
 export default {
   name: 'Chocolates_products',
   props: {
     msg: String
   },
-  data() {
-    return {
-      products: [
-        {
-          id: 1,
-          title: "chocolat blanc",
-          price: 15 + " €",
-          img: chocoBlanc,
-          total: 0
-        },
-        {
-          id: 2,
-          title: "chocolat noir",
-          price: 23 + " €",
-          img: chocoBlack,
-          total: 0
-        },
-        {
-          id:3,
-          title: "chocolat lait",
-          price: 12 + " €",
-          img: chocoMilk,
-          total: 0
-        }
-      ],
-      
+  computed: {
+    products(){
+      return this.$store.getters.getProducts
     }
-    
   },
   
 }
